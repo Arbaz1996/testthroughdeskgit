152f9db96a8221a652c228a5e47a16d7cda0d001
Arbaz1996 <arbaz.seedout@gmail.com>
2022-08-08 18:24:57 +0500

    2nd

--- testthroughdeskgit/src/testthroughdeskgit/testthroughdeskgit.java
+++ testthroughdeskgit/src/testthroughdeskgit/testthroughdeskgit.java
@@ -5,6 +5,7 @@ public class testthroughdeskgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("3332dcsd");
+		System.out.println("123 test");
 	}
 
 }
